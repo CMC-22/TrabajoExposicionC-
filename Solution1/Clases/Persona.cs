e8ee922f3182b9035ed8a21830f096fc7ff69fbe
Carolina <carolina22cmc@gmail.com>
2024-08-05 22:52:04 -0500

    se sube lo de la primera exposicion

---
+++ Solution1/Clases/Persona.cs
@@ -0,0 +1,52 @@
+﻿namespace Clases
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Direccion { get; set; }
+
+
+        public Persona(string nombre, int edad, string direccion)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Direccion = direccion;
+        }
+
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Edad: {Edad}");
+            Console.WriteLine($"Dirección: {Direccion}");
+        }
+
+
+        public bool EsMayorDeEdad()
+        {
+            return Edad >= 18;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Persona persona1 = new Persona("Carolina Martinez", 16, "Campoalegre");
+
+            persona1.MostrarInformacion();
+
+
+            if (persona1.EsMayorDeEdad())
+            {
+                Console.WriteLine("La persona es mayor de edad.");
+            }
+            else
+            {
+                Console.WriteLine("La persona es menor de edad.");
+            }
+        }
+    }
+}
---
+++ Solution1/Interfaces/IAnimal.cs
@@ -0,0 +1,51 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface IAnimal
+    { 
+    string Nombre { get; set; }
+
+    //Metodo que debe ser implementado por las clases
+    void HacerSonido();
+}
+
+public class Perro : IAnimal
+{
+    public string Nombre { get; set; }
+
+    public void HacerSonido()
+    {
+        Console.WriteLine("El perro hace: ¡Gua!");
+    }
+}
+
+public class Gato : IAnimal
+{
+    public string Nombre { get; set; }
+    public void HacerSonido()
+    {
+        Console.WriteLine("El gato hace: ¡Miau!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IAnimal miPerro = new Perro { Nombre = "Shagy" };
+        IAnimal miGato = new Gato { Nombre = "Salem" };
+
+        miPerro.HacerSonido();
+        miGato.HacerSonido();
+
+        Console.WriteLine($"Nombre del perro: {miPerro.Nombre}");
+        Console.WriteLine($"Nombre del gato: {miGato.Nombre}");
+    }
+}
+}
